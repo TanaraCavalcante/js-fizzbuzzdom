861a881fad78c928d2b3f394064d1ca4c51e76a8
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-02 17:09:32 -0400

    HTML - CSS

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,9 @@
-// Recuper os elementos do DOM
-//escrever em HTML 
+//Escrever em HTML 
+// Recuperar os elementos do DOM
 //Criar um lista con "for" em JS
 //definir os multiplo de 3
 //definir os multiplos de 5
 //definir os multiplos de 3 e 5 coincidentes
+
+//TODO  Recuperare gli elementi del DOM
+
