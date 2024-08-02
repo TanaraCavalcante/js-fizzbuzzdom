357ccc0e88128daa9490de0e7080b9a18d63db85
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-02 16:56:00 -0400

    check the stages

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,6 @@
-console.log('JS ok')
+// Recuper os elementos do DOM
+//escrever em HTML 
+//Criar um lista con "for" em JS
+//definir os multiplo de 3
+//definir os multiplos de 5
+//definir os multiplos de 3 e 5 coincidentes
